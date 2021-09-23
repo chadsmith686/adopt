a2c18f35ad3c8839c6c83a8d57e40a2cfb11eba7
Chad Smith <chadsmithmusic@icloud.com>
2021-09-22 19:40:33 -0700

    initialized project, added 'Pet' model

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///pet_db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
